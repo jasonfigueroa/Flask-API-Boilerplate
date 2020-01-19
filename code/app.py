4fff5062c55fff63d3b3755829dfc3a1d085ed53
Jason Figueroa <jason.figueroa2@gmail.com>
2020-01-18 21:22:02 -0600

    updated dependencies, added more resources, locked down endpoints

--- code/app.py
+++ code/app.py
@@ -1,17 +1,20 @@
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
-from flask_cors import CORS
+# from flask_cors import CORS
 
 from security import authenticate, identity
-from resources.user import UserRegister, UsernameExists
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'test' # omit this line if publishing this source code to a 
 						# public location, also change this secret key
-app.config['CORS_HEADERS'] = 'Content-Type'
+# app.config['CORS_HEADERS'] = 'Content-Type'
 # cors = CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 api = Api(app)
 
@@ -21,8 +24,12 @@ def create_tables():
 
 jwt = JWT(app, authenticate, identity)
 
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
-api.add_resource(UsernameExists, '/usernameexists/<username>')
+
 
 if __name__ == '__main__':
 	from db import db
